02e59d854d70f298439a0f70c7b29028e1162931
ibrahimAlikilic <ibrahim.alikilic@gmail.com>
2024-10-26 13:37:47 +0300

    Blynk
    Blynk ile bağlantı kurulup led akıldı ve sıcaklık verisi alındı

---
+++ Blynk_Baglanti/Blynk_Baglanti.ino
@@ -0,0 +1,44 @@
+/*********************************************************
+BSM313 Nesnelerin İnterneti ve Uygulamaları Dersi
+Prof. Dr. Cüneyt BAYILMIŞ
+Blynk 2.0 IoT Genel Uygulama | Arduino IDE Kodları
+*********************************************************/ 
+#define BLYNK_PRINT Serial 
+#include <ESP8266WiFi.h> 
+#include <BlynkSimpleEsp8266.h> 
+char auth [] = "";  // Blynk uygulaması tarafından, mailinize gelen token key    
+char ssid [] = "";  // Bağlantı yapacağınız Wi‐Fi adı 
+char pass [] = ""; // Wi‐Fi şifreniz 
+// Sanal pin oluşturma
+BLYNK_WRITE(V0) { 
+  digitalWrite(D0, param.asInt()); 
+}
+/*
+BLYNK_WRITE(V0): Blynk platformunda bir sanal pin (V0) üzerine gelen veriyi almak için kullanılan özel bir fonksiyondur.
+V0, Blynk uygulamasındaki sanal bir pin olarak çalışır ve Arduino ile uygulama arasında veri aktarımı sağlar.
+*/
+void setup() 
+{ 
+  Serial.begin(9600); 
+  pinMode(D0,OUTPUT); 
+  Blynk.begin(auth, ssid, pass, "blynk.cloud",80); 
+}
+/*
+Serial.begin(9600);: Seri iletişimi başlatır ve haberleşme hızını 9600 baud olarak ayarlar. Bu, hata ayıklama veya sensör verilerini görmek için kullanılır.
+pinMode(D0, OUTPUT);: D0 pinini çıkış (OUTPUT) modunda ayarlar. Bu sayede bu pine bağlı bir LED’i açıp kapatabilirsiniz. // D0 pini + ucu
+Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);: Blynk sunucusuna bağlanır. Bu komutun parametreleri:
+auth: Blynk uygulaması tarafından verilen kimlik doğrulama kodu (auth token).
+ssid: Wi-Fi ağının adı.
+pass: Wi-Fi şifresi.
+"blynk.cloud": Blynk’in sunucu adresi (varsayılan bulut adresi).
+80: Kullanılan port numarasıdır.
+*/
+void loop() 
+{ 
+  Blynk.run(); 
+}
+/*
+Blynk.run();: Blynk kütüphanesinin sürekli çalışmasını sağlar.
+Bu, Blynk platformu ile Arduino arasında sürekli iletişim sağlar.
+Diğer işlevler gibi çalışması gereken Blynk ile ilgili tüm işlemleri burada işler.*/
+
